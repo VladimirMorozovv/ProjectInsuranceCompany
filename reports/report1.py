351dea98fc1a63223a327582439b9fa8da0bbfaf
daradegtareva <vovakaqq@gmail.com>
2022-08-28 15:58:56 +0300

    add report

--- reports/report1.py
+++ reports/report1.py
@@ -1,9 +1,12 @@
 import config
 import mysql.connector
 
+class :
+    def __init__(self, data_start, data_stop):
+        self.data_start = data_start
+        self.data_stop = data_stop
 
-class Profitability:
-    def __init__(self):
+    def processing_report(self, object_type: data_object_type.ObjectType):
         try:
             with mysql.connector.connect(host=config.host,
                                          user=config.user,
@@ -11,9 +14,9 @@ class Profitability:
                                          database=config.database,
                                          ) as connection:
                 select_profitability = "SELECT OUTPUT.sum - INPUT.sum AS 'Разница между проданными полисами и выплат" \
-                                      " страхователям' FROM (SELECT SUM(costInsurance) sum FROM insurancePolicyClient " \
-                                      "WHERE status='sold') OUTPUT, (SELECT SUM(payoutAmount) sum " \
-                                      "FROM payoutsDirectoryClient) INPUT"
+                                       " страхователям' FROM (SELECT SUM(costInsurance) sum FROM insurancePolicyClient " \
+                                       "WHERE status='sold') OUTPUT, (SELECT SUM(payoutAmount) sum " \
+                                       "FROM payoutsDirectoryClient) INPUT"
                 with connection.cursor() as cursor:
                     cursor.execute(select_profitability)
                     result = cursor.fetchall()
@@ -22,12 +25,3 @@ class Profitability:
 
         except Exception as e:
             self.mistake = e
-
-
-
-
-    def profitability(self, difference):
-        if difference >= 0:
-            return f'Прибыль компании составляет {difference}'
-        elif difference < 0:
-            return f'Убыток компании составляет {difference}'
